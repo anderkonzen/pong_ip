bfbb20188e130c8ebaa4407299fd9762fb244e11
Anderson Konzen <anderson@anderkonzen.com>
2018-09-21 22:11:56 -0300

    Inspect conn headers

--- lib/pong_ip_web/controllers/pong_controller.ex
+++ lib/pong_ip_web/controllers/pong_controller.ex
@@ -2,7 +2,9 @@ defmodule PongIpWeb.PongController do
   use PongIpWeb, :controller
 
   def pong(conn, params) do
+    IO.puts "remote IP"
     IO.inspect conn.remote_ip
+    IO.puts "headers"
     IO.inspect conn.req_headers
     # require IEx; IEx.pry
     ip = to_string(:inet.ntoa(conn.remote_ip))
